c0081c00af5e14d048b827bfaf18975c831a7cf5
Anmol R. Srivastava <91518669+arssite@users.noreply.github.com>
2024-05-21 14:17:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.write("Firstly Click on Embedding to Vectorize the Backend Document So that G
 st.write("Write Queries and press \"Enter\"")
 url="https://github.com/arssite"
 url2="https://www.linkedin.com/in/anmol-r-srivastava"
-st.markdown("for more [Github]%s",url)
+st.markdown("for more [Github](%s)",url)
 llm=ChatGroq(groq_api_key=groq_api_key,
              model_name="Llama3-8b-8192")
 
